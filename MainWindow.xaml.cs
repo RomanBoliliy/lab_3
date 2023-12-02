1a3ce4586c7bdfe119d05e7d7e45042e7b2abe92
RomanBoliliy <21fi.r.bolilyi@std.npu.edu.ua>
2023-12-02 20:48:14 +0200

    Program without DB

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection _connection = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Vhid_Click(object sender, RoutedEventArgs e)
+        {
+            SignIn signIn = new SignIn();
+            signIn.Show();
+            Hide();
+
+        }
+
+        private void Button_Regester_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            signUp.Show();
+            Hide();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+
+            _connection.Open();
+            if (_connection.State == ConnectionState.Open) 
+            {
+                MessageBox.Show("Connection opened");
+            }
+
+        }
+    }
+}
---
+++ SignUp.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_SignUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
